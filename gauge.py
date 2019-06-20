24f969b46a89ed6809ec91065ffe93626c1675f3
judoal <allengordon@randori-consulting.com>
2019-06-20 11:40:46 -0600

    test code

---
+++ gauge.py
@@ -0,0 +1,23 @@
+import PIL
+
+from PIL import Image
+
+percent = 20  # Percent for gauge
+output_file_name = 'new_gauge.png'
+
+# X and Y coordinates of the center bottom of the needle starting from the top left corner
+#   of the image
+x = 825
+y = 825
+loc = (x, y)
+
+percent = percent / 100
+rotation = 180 * percent  # 180 degrees because the gauge is half a circle
+rotation = 90 - rotation  # Factor in the needle graphic pointing to 50 (90 degrees)
+
+dial = Image.open('needle.png')
+dial = dial.rotate(rotation, resample=PIL.Image.BICUBIC, center=loc)  # Rotate needle
+
+gauge = Image.open('gauge.png')
+gauge.paste(dial, mask=dial)  # Paste needle onto gauge
+gauge.save(output_file_name)
